1afe40798f99402a98eaf4c097e90cab65854848
victorialdp <53184730+victorialdp@users.noreply.github.com>
2019-10-11 10:55:10 -0400

    Update Final.java

--- Final.java
+++ Final.java
@@ -6,27 +6,27 @@ public class Final {
 
   //generateSign
     public static String generateSign(String month, int day) {
-      if (month.isEqual("January") && day >= 20 || month.isEqual("February") && day <= 18) {
+      if (month.equals("January") && day >= 20 || month.equals("February") && day <= 18) {
         return "Aquarius";
-      } else if (month.isEqual("February") || month.isEqual("March") && day <= 20) {
+      } else if (month.equals("February") || month.equals("March") && day <= 20) {
         return "Pisces";
-      } else if (month.isEqual("March") || month.isEqual("April") && day <= 19) {
+      } else if (month.equals("March") || month.equals("April") && day <= 19) {
         return "Aries";
-      } else if (month.isEqual("April") || month.isEqual("May") && day <= 20) {
+      } else if (month.equals("April") || month.equals("May") && day <= 20) {
         return "Taurus";
-      } else if (month.isEqual("May") || month.isEqual("June") && day <= 20) {
+      } else if (month.equals("May") || month.equals("June") && day <= 20) {
         return "Gemini";
-      } else if (month.isEqual("June") || month.isEqual("July") && day <= 22) {
+      } else if (month.equals("June") || month.equals("July") && day <= 22) {
         return "Cancer";
-      } else if (month.isEqual("July") || month.isEqual("August") && day <= 22) {
+      } else if (month.equals("July") || month.equals("August") && day <= 22) {
         return "Leo";
-      } else if (month.isEqual("August") || month.isEqual("September") && day <= 22) {
+      } else if (month.equals("August") || month.equals("September") && day <= 22) {
         return "Virgo";
-      } else if (month.isEqual("September") || month.isEqual("October") && day <= 22) {
+      } else if (month.equals("September") || month.equals("October") && day <= 22) {
         return "Libra";
-      } else if (month.isEqual("October") || month.isEqual("November") && day <= 21) {
+      } else if (month.equals("October") || month.equals("November") && day <= 21) {
         return "Scorpio";
-      } else if (month.isEqual("November") || month.isEqual("December") && day <= 21) {
+      } else if (month.equals("November") || month.equals("December") && day <= 21) {
         return "Sagittarius";
       } else {
         return "Capricorn";
