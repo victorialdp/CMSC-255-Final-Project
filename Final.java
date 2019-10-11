3c55c514d5653e2fbbafbecbb26c88e54cab3bb0
victorialdp <53184730+victorialdp@users.noreply.github.com>
2019-10-11 10:13:44 -0400

    Update Final.java

--- Final.java
+++ Final.java
@@ -2,18 +2,15 @@ public class Final {
   public static void main(String[] args) {
 
   // main method
-
+  }
 
   //generateSign
-    
     public static String generateSign(String month, int day) {
       if (month.isequal("January")
     }
 
   // generateHoroscope
 
-
-  
   public static String generateHoroscope() {
   
     // generateHoroscope
