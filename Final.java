f0de57e9fbd07ac843d544c00c351505284c1096
geringjl <54373822+geringjl@users.noreply.github.com>
2019-10-25 11:20:18 -0400

    Update Final.java

--- Final.java
+++ Final.java
@@ -1,11 +1,21 @@
+
+java.util.Scanner
+
 public class Final {
   public static void main(String[] args) {
+  Scanner input = new Scanner(System.in);
 
-  // main method
-
-  //generateSign
-
-  // generateHoroscope
+    System.out.println("Enter a birth month: ");
+    String month = input.nextLine();
+    System.out.println("Enter a a birth day: ");
+    int day = input.nextInt();
+    
+    String sign = generaterSign(month, day);
+    String horoscope = generateHoroscope();
+    
+    System.out.println(sign + " " + horoscope);
+    
+    
 
   }
 }
