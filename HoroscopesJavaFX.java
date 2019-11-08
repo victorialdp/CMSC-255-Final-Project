abf7a221610891a1445a2a9fff27e483c0769332
syoungstone <53226974+syoungstone@users.noreply.github.com>
2019-11-07 19:13:13 -0500

    Update HoroscopesJavaFX.java

--- HoroscopesJavaFX.java
+++ HoroscopesJavaFX.java
@@ -30,37 +30,61 @@ public class HoroscopesJavaFX extends Application {
     // Creates String and Integer arrays of all possible month and day options
     private String[] months = {"January", "February", "March", "April", "May", "June",
             "July", "August", "September", "October", "November", "December"};
-    private Integer[] days = {1 , 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16,
+    private Integer[] days31 = {1 , 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16,
             17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31};
-    
-    // Creates a ComboBox for months and for days (dayBox must use Integer and not int for this to work)
-    private ComboBox<String> monthBox = new ComboBox<>();
-    private ComboBox<Integer> dayBox = new ComboBox<>();
+    private Integer[] days30 = {1 , 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16,
+            17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30};
+    private Integer[] days29 = {1 , 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16,
+            17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29};
 
     // Main body of JavaFX program
     @Override
     public void start(Stage primaryStage) {
-        
-        // Attaches the contents of months and days to monthBox and dayBox 
-        monthBox.getItems().addAll(FXCollections.observableArrayList(months));
-        dayBox.getItems().addAll(FXCollections.observableArrayList(days));
 
-        // Creates an Image using a picture representing the Aries star sign from an astrology website
-        Image starSign = new Image("http://www.astrology-insight.com/astrology/arisbigbutton.gif");
+        // Creates a ComboBox for months and for days (dayBox must use Integer and not int for this to work)
+        ComboBox<String> monthBox = new ComboBox<>(FXCollections.observableArrayList(months));
+        ComboBox<Integer> dayBox = new ComboBox<>(FXCollections.observableArrayList(days31));
 
-        // Sets up a subpane with both comboboxes, corresponding Labels, and a button to run the program
+        // Sets default starting values for monthBox and dayBox
+        monthBox.setValue("January");
+        dayBox.setValue(1);
+
+        // Sets number of days to match the month
+        monthBox.setOnAction(e -> {
+            if ("February".equals(monthBox.getValue())) {
+                dayBox.setItems(FXCollections.observableArrayList(days29));
+            }
+            else if ("SeptemberAprilJuneNovember".contains(monthBox.getValue())) {
+                dayBox.setItems(FXCollections.observableArrayList(days30));
+            }
+            else {
+                dayBox.setItems(FXCollections.observableArrayList(days31));
+            }
+        });
+
+        // Creates a starting message and image using a picture from an astrology website
+        Label horoscope = new Label("Welcome to the Horoscope Wizard! Enter your birthday to learn your destiny.");
+        ImageView starSign = new ImageView("Signs\\StarCluster.jpg");
+
+        // Creates a button to compute the horoscope with an action handler
+        Button computeButton = new Button("Compute Horoscope");
+        computeButton.setOnAction(e -> {
+
+            // Replaces bottom text with a randomly generated horoscope
+            horoscope.setText(Horoscopes.generateHoroscope());
+
+            // Calls generateSign on the selected month and day, then updates the image to match the resulting sign
+            starSign.setImage(new Image("Signs\\" + Horoscopes.generateSign(monthBox.getValue(), dayBox.getValue()) + ".gif"));
+        });
+
+        // Sets up a subpane with both comboboxes, corresponding Labels, and the compute button
         FlowPane inputPane = new FlowPane();
         inputPane.setHgap(5);
         inputPane.getChildren().add(new Label("Enter birth month:"));
         inputPane.getChildren().add(monthBox);
         inputPane.getChildren().add(new Label("Enter birth day:"));
         inputPane.getChildren().add(dayBox);
-        inputPane.getChildren().add(new Button("Compute Horoscope"));
-
-        // Creates a Label containing a sample horoscope, alters its font and format, and wraps the text
-        Label horoscope = new Label("You will pick up a tall, handsome hitchhiker off the side of I-95 on your way" +
-                " to see the Ravens play in Baltimore. The two of you will fall in love while eating lunch at" +
-                " a Denny's in Fredericksburg and miss the game, which the Ravens will lose 12-97.");
+        inputPane.getChildren().add(computeButton);
         horoscope.setFont(Font.font("Times New Roman", FontWeight.BOLD, FontPosture.ITALIC, 20));
         horoscope.setWrapText(true);
 
@@ -68,13 +92,16 @@ public class HoroscopesJavaFX extends Application {
         BorderPane pane = new BorderPane();
         pane.setPadding(new Insets(10));
         pane.setTop(inputPane);
-        pane.setCenter(new ImageView(starSign));
+        pane.setCenter(starSign);
         pane.setBottom(horoscope);
 
         // Incorporates the master pane into a Scene with a set size and title
         Scene scene = new Scene(pane, 500, 350);
         primaryStage.setTitle("Horoscope Wizard");
         primaryStage.setScene(scene);
+
+        // Prevents user from resizing the Stage and then generates the stage
+        primaryStage.setResizable(false);
         primaryStage.show();
 
     }
@@ -83,4 +110,5 @@ public class HoroscopesJavaFX extends Application {
     public static void main(String[] args) {
         launch(args);
     }
+
 }
